0201111113ea93061dae563c98b94e05956fc8c4
Amir <amirrllotfi@gmail.com>
2025-03-13 10:33:34 +0100

    📌 Added service locator using GetIt
    Integrated GetIt for dependency injection.
    Registered necessary services and repositories.
    Ensured singleton/lazy initialization for efficient resource management.

--- lib/featuers/feature_intro/bloc/splash/splash_cubit.dart
+++ lib/featuers/feature_intro/bloc/splash/splash_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:ustore/featuers/feature_intro/usecase/usecase_splash.dart';
 
 part 'splash_state.dart';
 part 'connection_status.dart';
 
 class SplashCubit extends Cubit<SplashState> {
-  SplashCubit() : super(SplashState(connectionStatus: ConnectionInitial()));
+  UsecaseSplash usecaseSplash;
+  SplashCubit({required this.usecaseSplash})
+      : super(SplashState(connectionStatus: ConnectionInitial()));
+
+  void checkConnectionEvent() {
+    emit(state.copyWith(newConnectionStatus: ConnectionInitial()));
+    usecaseSplash.checkConnectionEvent().then((value) {
+      if (value) {
+        emit(state.copyWith(newConnectionStatus: ConnectionOn()));
+      } else {
+        emit(state.copyWith(newConnectionStatus: ConnectionOff()));
+      }
+    });
+  }
 }
