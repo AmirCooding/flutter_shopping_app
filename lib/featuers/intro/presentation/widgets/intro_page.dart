ddad596030192aa69dd8841e9520c114ff812118
Amir <amirrllotfi@gmail.com>
2025-03-14 11:40:36 +0100

    Implement position handling for title and description in intro page
    - Adjusted positioning logic for title and description
    - Ensured proper alignment inside the intro main wrapper
    - Improved responsiveness and layout consistency

---
+++ lib/featuers/intro/presentation/widgets/intro_page.dart
@@ -0,0 +1,43 @@
+import 'package:delayed_widget/delayed_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:ustore/common/constant/theme_helper.dart';
+import 'package:ustore/theme/app_colors.dart';
+import 'package:ustore/theme/app_font.dart';
+
+class Intropage extends StatelessWidget {
+  final String title;
+  final String description;
+  const Intropage({super.key, required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode = ThemeHelper.isDarkMode(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          DelayedWidget(
+              delayDuration: const Duration(milliseconds: 500),
+              animationDuration: const Duration(seconds: 1),
+              animation: DelayedAnimations.SLIDE_FROM_BOTTOM,
+              child: Text(title,
+                  style:
+                      isDarkMode ? AppFont.darkHeading : AppFont.lightHeading)),
+          SizedBox(height: 20),
+          DelayedWidget(
+              delayDuration: const Duration(milliseconds: 500),
+              animationDuration: const Duration(seconds: 1),
+              animation: DelayedAnimations.SLIDE_FROM_RIGHT,
+              child: Shimmer.fromColors(
+                  baseColor: isDarkMode ? AppColors.greyLight : AppColors.grey,
+                  highlightColor: isDarkMode
+                      ? AppColors.textSecondary
+                      : AppColors.textBlack,
+                  child: Text(description, style: AppFont.lightBody))),
+        ],
+      ),
+    );
+  }
+}
