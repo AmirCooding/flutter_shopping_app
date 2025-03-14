f926e96939d899b410e0215a99676520e92214b6
Amir <amirrllotfi@gmail.com>
2025-03-14 12:17:23 +0100

    fix: Optimize GetStartBtn animation and state management
    - Fixed incorrect initialization of AnimationController in setState
    - Moved AnimationController setup to initState for proper lifecycle handling
    - Ensured smooth fade-in effect using CurvedAnimation
    - Improved memory management by disposing of the controller properly
    - Maintained dark and light mode color support for better UI consistency

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:ustore/common/utils/widgets/loading_screen.dart';
 import 'package:ustore/common/utils/widgets/no_internent.dart';
 import 'package:ustore/featuers/home/presentation/screens/home_screen.dart';
 import 'package:ustore/featuers/intro/presentation/bloc/splash/splash_cubit.dart';
+import 'package:ustore/featuers/intro/presentation/screens/intro_main_wrapper.dart';
 import 'package:ustore/featuers/intro/presentation/screens/splash_screen.dart';
 import 'package:ustore/firebase_options.dart';
 import 'package:ustore/setup_locator.dart';
@@ -27,12 +28,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/splash',
+      initialRoute: IntroMainWrapper.introMainWrapper,
       routes: {
-        SplashScreen.routeName: (context) => const SplashScreen(),
-        HomeScreen.routeName: (context) => const HomeScreen(),
-        NoInternet.routeName: (context) => const NoInternet(),
-        LoadingScreen.routeName: (context) => const LoadingScreen(),
+        SplashScreen.splash: (context) => const SplashScreen(),
+        HomeScreen.home: (context) => const HomeScreen(),
+        NoInternet.nointernet: (context) => const NoInternet(),
+        LoadingScreen.loading: (context) => const LoadingScreen(),
+        IntroMainWrapper.introMainWrapper: (context) =>
+            const IntroMainWrapper(),
       },
       debugShowCheckedModeBanner: false,
       title: 'UStore',
