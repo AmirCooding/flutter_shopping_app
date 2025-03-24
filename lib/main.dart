1b00534adac67a63daeaf080636c8236b45a7250
Amir <amirrllotfi@gmail.com>
2025-03-24 18:28:41 +0100

    Create MyApp Class and call it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/main.dart';
 import 'package:ustore/common/constant/language_manager.dart';
-import 'package:ustore/common/utils/widgets/loading_screen.dart';
-import 'package:ustore/common/utils/widgets/no_internent.dart';
-import 'package:ustore/featuers/home/presentation/screens/home_screen.dart';
 import 'package:ustore/featuers/intro/presentation/bloc/intro/intro_cubit.dart';
 import 'package:ustore/featuers/intro/presentation/bloc/splash/splash_cubit.dart';
-import 'package:ustore/featuers/intro/presentation/screens/intro_main_wrapper.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:ustore/featuers/intro/presentation/screens/splash_screen.dart';
 import 'package:ustore/firebase_options.dart';
-import 'package:ustore/setup_locator.dart';
-import 'package:ustore/config/theme/app_theme.dart';
+import 'package:ustore/locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await firbaseinit();
   await LanguageManager().loadLanguage();
   setupLocator();
   runApp(MultiBlocProvider(
@@ -34,36 +26,8 @@ void main() async {
   ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      locale: const Locale('en', 'US'),
-      supportedLocales: const [
-        Locale('en', 'US'),
-        Locale('de', 'DE'),
-      ],
-      // Routes
-      initialRoute: SplashScreen.splash,
-      routes: {
-        SplashScreen.splash: (context) => const SplashScreen(),
-        HomeScreen.home: (context) => const HomeScreen(),
-        NoInternet.nointernet: (context) => const NoInternet(),
-        LoadingScreen.loading: (context) => const LoadingScreen(),
-        IntroMainWrapper.introMainWrapper: (context) => IntroMainWrapper(),
-      },
-      debugShowCheckedModeBanner: false,
-      title: 'UStore',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-    );
-  }
+Future<void> firbaseinit() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
